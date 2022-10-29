269c111ca825804998540f56dff244348bcf475f
TahjD <tahjjobs@gmail.com>
2022-10-29 08:32:05 -0700

    html+css 01

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,3 +1,33 @@
+
+// Create a randomly generated # between 1-100 that will be the answer //
+const msg1 = document.getElementById("msg1");
+const msg2 = document.getElementById("msg2");
+const msg3 = document.getElementById("msg3");
+
+var answer = Math.floor(Math.random()*100) + 1;
+var numberOfGuesses = 0;
+var guessedNums = [];
+
+function play (){
+    var user_entries = document.getElementById("guess").value 
+
+    if (user_entries <1 || user_entries > 100){
+        alert('Read the Directions -_-')
+    }
+}
+
+
+
+function randomGenNum () {
+
+}
+
+
+
+// 
+
+
+
 /* 
 
 Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
