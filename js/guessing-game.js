da252e4e626e592173b1e5f8ff43de23b14f8a4a
TahjD <tahjjobs@gmail.com>
2022-10-30 09:52:22 -0700

    Working Without EndGame Func

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,5 +1,3 @@
-
-// Create a randomly generated # between 1-100 that will be the answer //
 const msg1 = document.getElementById("msg1");
 const msg2 = document.getElementById("msg2");
 const msg3 = document.getElementById("msg3");
@@ -7,33 +5,56 @@ const msg3 = document.getElementById("msg3");
 var answer = Math.floor(Math.random()*100) + 1;
 var numberOfGuesses = 0;
 var guessedNums = [];
+var resetBut;
+function game(){
+    var user_entries = document.getElementById("guess").value
 
-function play (){
-    var user_entries = document.getElementById("guess").value 
+    if(user_entries < 1 || user_entries > 100) {
+        alert("Read The Directions");
+    } else {
+        guessedNums.push(user_entries);
+        numberOfGuesses += 1;
 
-    if (user_entries <1 || user_entries > 100){
-        alert('Read the Directions -_-')
-    }
+    } if(user_entries < answer){
+        msg1.textContent = "too low";
+        msg2.textContent = "Number of Guesses " + numberOfGuesses;
+        msg3.textContent = "Guessed Numbers " + guessedNums;
+
+
+    } else if (user_entries > answer){
+        msg1.textContent = "too high"
+        msg2.textContent = "Number of Guesses " + numberOfGuesses;
+        msg3.textContent = "Guessed Numbers " + guessedNums;
+
+
+    } else if (user_entries === answer){
+        msg1.textContent = "YOU GOT IT";
+        msg2.textContent = "YOU GOT IT IN " + numberOfGuesses
 }
+// user_entries.addEventListener('click',game);
 
+// function endGame(){
+//     user_entries.disabled=true; 
+//     guess.disabled=true;
+//     resetBut = document.createElement('button'); resetBut.textContent = 'Play Again!?';
+//  document.body.appendChild(resetBut);
+// resetBut.addEventListener('click',redoGame);
+// }
+// function redoGame (){
+//      numberOfGuesses = 1;
+//      var resetGo = document.querySelector('.resultParas');
+//      for (var i = 0; i < redoGame.length; i++){
+//         resetGo[i].textContent = "";
+        
+//      }
+//      resetBut.parentNode.removeChild(resetBut);
+//      guess.disabled = false;
+//      user_entries.disabled = false;
+//      guess.value = '';
+//      guess.focus();
 
+     
+// }
 
-function randomGenNum () {
 
 }
-
-
-
-// 
-
-
-
-/* 
-
-Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
-by running "npm test".
-
-In this file, you will also include the event listeners that are needed to interact with your HTML file when
-a user clicks a button or adds a guess to the input field.
-
-*/
